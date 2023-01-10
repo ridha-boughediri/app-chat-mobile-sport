7608e6e705546c7e6f4a9e04428d31ee5654798b
yonathan-darmon <yonathan.darmon@laplateforme.io>
2023-01-10 16:40:22 +0100

    login made

--- Reactnative/src/screens/LoginScreen/LoginScreen.js
+++ Reactnative/src/screens/LoginScreen/LoginScreen.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
+import { MaterialCommunityIcons } from '@expo/vector-icons/';
+import { root } from 'postcss';
 
 const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [token, setToken] = useState('');
+  const [rightIcon, setRightIcon] = useState('eye');
+  const [passwordVisibility, setPasswordVisibility] = useState(true);
 
+  const handlePasswordVisibility = () => {
+    if (rightIcon === 'eye') {
+      setRightIcon('eye-off');
+      setPasswordVisibility(!passwordVisibility);
+    } else if (rightIcon === 'eye-off') {
+      setRightIcon('eye');
+      setPasswordVisibility(!passwordVisibility);
+    }
+  };
   const handleLogin = async () => {
     try {
       let response = await axios.post('http://localhost:8888/auth/login', {
@@ -35,6 +48,10 @@ const LoginScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <Image
+        source={require('../../../assets/image/US_Sports_Insights-removebg-preview.png')}
+        style={{ width: 300, height: 300 }}
+      />
       <TextInput
         style={styles.input}
         value={email}
@@ -43,13 +60,19 @@ const LoginScreen = ({ navigation }) => {
         autoCapitalize='none'
         keyboardType='email-address'
       />
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-        placeholder='Mot de passe'
-        secureTextEntry={true}
-      />
+      <View
+        style={styles.input}>
+        <TextInput
+          style={styles.pass}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          placeholder='Mot de passe'
+          secureTextEntry={passwordVisibility}
+        />
+        <Pressable onPress={handlePasswordVisibility}>
+          <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
+        </Pressable>
+      </View>
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Connexion</Text>
       </TouchableOpacity>
@@ -78,12 +101,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     borderWidth: 1,
     borderColor: '#ccc',
-    borderRadius: 5
+    borderRadius: 15,
+    flexDirection: 'row',
   },
   button: {
     backgroundColor: '#0E64D2',
     padding: 10,
-    borderRadius: 5,
+    borderRadius: 10,
     width: '100%',
     alignItems: 'center'
   },
@@ -102,6 +126,9 @@ const styles = StyleSheet.create({
   },
   linkText: {
     color: '#1E90FF'
+  },
+  pass:{
+    width: '90%'
   }
 });
 export default LoginScreen;
