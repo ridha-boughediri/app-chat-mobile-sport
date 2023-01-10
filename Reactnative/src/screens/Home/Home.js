cda8eefeeab6281657ff7f19a5c2d9db96bce148
yonathan-darmon <yonathan.darmon@laplateforme.io>
2023-01-10 11:12:58 +0100

    ajust

--- Reactnative/src/screens/Home/Home.js
+++ Reactnative/src/screens/Home/Home.js
@@ -8,7 +8,7 @@ const HomeScreen = ({navigation}) => {
         source={require('../../../assets/image/US_Sports_Insights-removebg-preview.png')}
         style={{ width: 300, height: 300 }}
       />
-      <Text style={{ fontSize: 18, marginTop: 20, marginStart: 10, marginEnd: 10}}>
+      <Text style={{ fontSize: 18, marginTop: 20, marginStart: 10, marginEnd: 10, textAlign:"center"}}>
         Le Premier tchat pour discuter Pronostique et suivi de ses equipe  de sport americain favorites
       </Text>
       <View style={{ flexDirection: 'row', marginTop: 40 }}>
@@ -25,7 +25,7 @@ const HomeScreen = ({navigation}) => {
 const styles =  {
     button: {
     padding: 10,
-    backgroundColor: '#4CAF50',
+    backgroundColor: '#0E64D2',
     borderRadius: 5,
     marginRight: 10
   },
