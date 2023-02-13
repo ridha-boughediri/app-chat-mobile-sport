b778689f51a26be530c30ddb0e83ea395200de95
yonathan-darmon <yonathan.darmon@laplateforme.io>
2023-02-13 11:03:28 +0100

    admin

--- Reactnative/src/service/UserApi.js
+++ Reactnative/src/service/UserApi.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 
 async function findOneUser(id){
-    const res = await axios.get(`http://10.10.4.237:8888/users/register`);
+    const res = await axios.get(`http://10.10.20.160:8888/users/register`);
     return res.data;
 }
 
